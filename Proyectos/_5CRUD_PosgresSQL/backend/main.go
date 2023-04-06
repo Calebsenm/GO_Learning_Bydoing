932bd8a0fce2fb1bbd8b81b6520c80d5534be1f5
calebsenm <you@gmail.com>
2023-04-06 06:03:46 -0500

    test

--- Proyectos/_5CRUD_PosgresSQL/backend/main.go
+++ Proyectos/_5CRUD_PosgresSQL/backend/main.go
@@ -8,16 +8,15 @@ import (
 
 // server XD
 func main(){
-	fmt.Println("Server in localhost 8080")
+	fmt.Println("Server is running in localhost 8080")
 	// this is the golang server 
 	http.HandleFunc("/", func(w http.ResponseWriter, peticion *http.Request) {
 		io.WriteString(w, "welcome")
 	})
 
 
-	//
 
-	
+	//Server runing
 	Server := http.Server{
 		Addr: ":8080",
 	}
