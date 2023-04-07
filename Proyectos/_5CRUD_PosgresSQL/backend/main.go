3ff8eb4bec9fe00a18d45698bac3ef43c705fe1a
calebsenm <calebgamercraft@gmail.com>
2023-04-06 23:26:40 -0500

    Avances

--- Proyectos/_5CRUD_PosgresSQL/backend/main.go
+++ Proyectos/_5CRUD_PosgresSQL/backend/main.go
@@ -1,13 +1,39 @@
 package main
 
 import (
+	"database/sql"
 	"fmt"
-	"net/http"
 	"io"
+	"net/http"
+
+	_ "github.com/lib/pq"
 )
 
-func routes(){
-	
+const(
+	DB_HOST = "localhost"
+	DB_PORT = "5432"
+	DB_USER = "caleb"
+	DB_PASWORD = "12345"
+	DB_NAME = "Test"
+)
+
+func posgresqlConnect()  (*sql.DB , error ) {
+
+	postgresInfo :=  fmt.Sprintf("host=%s port=%d user=%s password=%s dbname=%s sslmode=%s",
+								 DB_HOST,DB_PORT,DB_USER, DB_PASWORD,DB_NAME)
+
+	db , err := sql.Open("postgresql", postgresInfo)
+
+	if err != nil{
+		panic(err)
+	}
+
+	err = db.Ping()
+  	if err != nil {
+    	panic(err)
+  	}
+
+	return db , err
 }
 
 
@@ -16,12 +42,12 @@ func main(){
 	fmt.Println("Server is running in localhost 8080")
 	// this is the golang server 
 	http.HandleFunc("/", func(w http.ResponseWriter, peticion *http.Request) {
-		io.WriteString(w, "welcome")
+		io.WriteString(w, "Aqui Vamos  XD ")
 	})
 	
 	//Server runing
 	Server := http.Server{
-		Addr: ":8080",
+		Addr: ":2080",
 	}
 
 	err1 := Server.ListenAndServe()
