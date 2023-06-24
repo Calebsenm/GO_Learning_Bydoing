4d79e35c0f44a5aff70b6c6a7a1947d661a05a27
calebsenm <calebgamercraft@gmail.com>
2023-06-23 23:54:11 -0500

    Terminado

---
+++ By_Example/_2Values/main.go
@@ -0,0 +1,20 @@
+
+
+package main 
+
+import "fmt"
+
+func main(){
+
+    fmt.Println("go"+"golang");
+
+    fmt.Println("1+1 = ", 1 + 1 );
+    fmt.Println("7 / 3", 7 / 3);
+
+    fmt.Println(true && false);
+    fmt.Println(true || false);
+    fmt.Println(!true);
+}
+
+
+
