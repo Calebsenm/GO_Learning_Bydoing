417685a6628ec9f0ea2564924a9d42aa78795b0a
calebsenm <calebgamercraft@gmail.com>
2023-09-07 13:56:54 +0000

    Data

---
+++ Code /A_Tour_Of_Go/Methods_Interfaces/ExerciseErrors_20.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+	"fmt"
+)
+
+type ErrNegativeSqrt float64
+
+func (e ErrNegativeSqrt) Error() string {
+	return fmt.Sprintf("Cannot  Sqrt  negative number: %v ", float64(e))
+}
+
+func Sqrt(x float64) (float64, error) {
+	if x < 0 {
+		return x , nil
+	}
+	
+	z := float64(0.1)
+
+	for {
+		z -= (z*z - x) / (2 * z)
+		sqrtData := z * z
+		fmt.Println(z, " = ", sqrtData)
+		if sqrtData == x {
+			return z , nil 
+		}
+	}
+
+}
+
+func main() {
+	valor , err := Sqrt(2)
+	
+	if err != nil{
+		fmt.Println(err)
+	}
+	fmt.Println(valor)
+}
