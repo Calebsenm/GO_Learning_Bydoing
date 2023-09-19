54e3e0db114cf88e6a7ecbba3b217c4ef05fe7f7
Caleb Seña Melo <91163501+Calebsenm@users.noreply.github.com>
2023-09-19 23:33:52 +0000

    Update

--- Code /A_Tour_Of_Go/Methods_Interfaces/Exercise_25/Exercice_Image_25.go
+++ Code /A_Tour_Of_Go/Methods_Interfaces/Exercise_25/Exercice_Image_25.go
@@ -1,32 +1,28 @@
 package main
 
 import (
-	"code.google.com/p/go-tour/pic"
-	"image"
-	"image/color"
+    "code.google.com/p/go-tour/pic"
+    "image"
+    "image/color"
 )
 
-type Image struct{}
-
-// ColorModel returns the Image's color model.
-func (i Image) ColorModel() color.Model {
-	return color.RGBAModel
+type Image struct {
+    x, y int
 }
 
-// Bounds returns the domain for which At can return non-zero color.
-// The bounds do not necessarily contain the point (0, 0).
-func (i Image) Bounds() image.Rectangle {
-	return image.Rect(0, 0, 255, 255)
+func (r *Image) Bounds() image.Rectangle {
+    return image.Rect(0, 0, r.x, r.y)
 }
 
-// At returns the color of the pixel at (x, y).
-// At(Bounds().Min.X, Bounds().Min.Y) returns the upper-left pixel of the grid.
-// At(Bounds().Max.X-1, Bounds().Max.Y-1) returns the lower-right one.
-func (i Image) At(x, y int) color.Color {
-	return color.RGBA{uint8(x + y), 255, 3, uint8(x)}
+func (r *Image) ColorModel() color.Model {
+    return color.RGBAModel
+}
+
+func (r *Image) At(x, y int) color.Color {
+    return color.RGBA{uint8(x), uint8(y), 255, 255}
 }
 
 func main() {
-	m := Image{}
-	pic.ShowImage(m)
+    m := &Image{256, 256}
+    pic.ShowImage(m)
 }
--- Code /A_Tour_Of_Go/Methods_Interfaces/Exercise_25/go.mod
+++ Code /A_Tour_Of_Go/Methods_Interfaces/Exercise_25/go.mod
@@ -1,3 +1,3 @@
-module image
+module example
 
 go 1.21.0
